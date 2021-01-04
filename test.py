6bd762b338ae0bae7ea52e459fdf6d5fdeef57f2
nghiavt2906 <nghiavt2906@gmail.com>
2021-01-04 09:28:15 +0700

    remove test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import configparser
-
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-config.set('DEFAULT', 'CURRENT_MAX_RECORD_ID', str(99999999))
-
-with open('config.ini', 'w') as configfile:
-    config.write(configfile)
